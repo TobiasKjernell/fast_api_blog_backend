a047ef595c0d7af5a270df0fe048c2ff4550d116
TobiasKjernell <tobiaskjernell@gmail.com>
2026-06-04 18:09:47 +0200

    router refactor

---
+++ routers/users.py
@@ -0,0 +1,116 @@
+import models
+from fastapi import  HTTPException, status, Depends, APIRouter
+from schemas import PostResponse, UserResponse, UserCreate, UserUpdate
+from typing import Annotated
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
+from database import get_db
+
+router = APIRouter()
+
+
+# Session: We create a session inside the db, Depends: Before we call the code, it will call the method inside Depends(fun).
+# DI works like middleware, but middlware calls on all routes on all requests, with DI we can call a specific route only
+# and then return this data back as the 'db' parameter 
+# Annotated just combines the type hint (Session) and extra metadata (Depends) into 1 parameter
+@router.post("", response_model=UserResponse, status_code=status.HTTP_201_CREATED, tags=['Users'])
+async def create_user(user: UserCreate, db: Annotated[AsyncSession, Depends(get_db)]):
+
+    result = await db.execute(select(models.User).where(models.User.username == user.username))
+    existing_user = result.scalars().first()
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username already exists"
+        )
+
+    result = await db.execute(select(models.User).where(models.User.email == user.email))
+    existing_email = result.scalars().first()
+    if existing_email:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email already exists"
+        )
+
+    new_user = models.User(
+        username = user.username,
+        email = user.email
+    )
+
+    db.add(new_user)
+    await db.commit()
+    await db.refresh(new_user)
+
+    return new_user
+
+
+@router.get('', tags=["Users"], response_model=list[UserResponse])
+async def get_users(db:Annotated[AsyncSession, Depends(get_db)]):
+    result = await db.execute(select(models.User))
+    users = result.scalars().all()
+    return users
+
+@router.get('/{user_id}', tags=["Users"], response_model=UserResponse)
+async def get_user(user_id:int, db: Annotated[AsyncSession, Depends(get_db)]):
+    result = await db.execute(select(models.User).where(models.User.id == user_id))       
+    user = result.scalars().first()
+
+    if user:
+        return user
+    
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+@router.get("/{user_id}/posts", tags=["Users"], response_model=list[PostResponse])
+async def get_user_posts(user_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
+    result = await db.execute(select(models.User).where(models.User.id == user_id))
+    user = result.scalars().first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    
+    result = await db.execute(select(models.Post).options(selectinload(models.Post.author)).where(models.Post.user_id == user_id))
+    posts = result.scalars().all()
+    return posts
+
+@router.patch("/{user_id}", tags=["Users"], response_model=UserResponse)
+async def update_user(user_id: int, user_update: UserUpdate, db: Annotated[AsyncSession, Depends(get_db)]):
+    result = await db.execute(select(models.User).where(models.User.id == user_id))
+    user = result.scalars().first()
+    # User checks
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+    if user_update.username is not None and user_update.username != user.username:
+        result = await db.execute(select(models.User).where(models.User.username == user_update.username))
+        existing_user = result.scalars().first()
+        if existing_user:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username already exists")
+    # Email checks
+    if user_update.email is not None and user_update.email != user.email:
+        result = await db.execute(select(models.User).where(models.User.email == user_update.email))
+        existing_email = result.scalars().first()
+        if existing_email:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
+    
+    if user_update.username is not None:
+        user.username = user_update.username
+    if user_update.email is not None:
+        user.email = user_update.email
+    if user_update.image_file is not None:
+        user.image_file = user_update.image_file
+
+    await db.commit()
+    await db.refresh(user)
+    return user
+
+
+@router.delete("/{user_id}", tags=["Users"], status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(user_id:int, db:Annotated[AsyncSession, Depends(get_db)]):
+    result = await db.execute(select(models.User).where(models.User.id == user_id))
+    user = result.scalars().first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    
+    await db.delete(user)
+    await db.commit()
